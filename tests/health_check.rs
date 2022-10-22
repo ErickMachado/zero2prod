3f5248bc9ad054554dbc68bf863e1b0c5070855b
Erick Machado <erick.gsmachado@gmail.com>
2022-10-22 01:45:00 -0300

    add integration test for GET /health_check route

---
+++ tests/health_check.rs
@@ -0,0 +1,25 @@
+use zero2prod::run;
+
+fn spawn_app() {
+    let server = run().expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
